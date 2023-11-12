90e748147130b0d68dbac824c38356dfba025570
Leahhha <jy4726@nyu.edu>
2023-11-12 15:46:40 -0500

    Update improviser-app.js

--- improviser-app.js
+++ improviser-app.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const cron = require('node-cron')
 const Pushover = require('node-pushover');
 
-cron.schedule('*/5 * * * * *', notify)
+cron.schedule('* * */2 * * *', notify)
 
 var randomNum;
 var fs = require('fs');
